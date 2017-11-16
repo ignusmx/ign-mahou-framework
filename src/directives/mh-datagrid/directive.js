ce5a3d9892e9a3358e72195e044e48e6ccf0d83f
zvhit002 <tprado@palig.com>
2017-11-16 03:27:26 -0600

    added transclude and isolate scope

--- src/directives/mh-datagrid/directive.js
+++ src/directives/mh-datagrid/directive.js
@@ -1,92 +1,10 @@
 angular.module('mahou').directive('mhDatagrid', function ( $compile ) {
   return {
     restrict: 'E',
-    scope: true,
+    scope: { config : '='},
     transclude : true,
-    compile : function(elem, attrs, ctrl, transclude)
+    link: function(scope, el, attrs, ctrl, transclude)
     {
-        var row = elem.find(".mh-datagrid-row");
-        row.attr("ng-repeat", "row in config.collection");
-
-        var col = elem.find(".mh-datagrid-col");
-        col.attr("ng-repeat", "column in config.columns");
-
-        var columnName = elem.find(".mh-datagrid-col-name");
-        columnName.html("{{column.name}}");
-
-        var rowData = elem.find(".mh-datagrid-row-text");
-        rowData.attr("ng-if","column.type != 'image'")
-        rowData.html("{{controller.selectAllModel}}");
-
-        var rowData = elem.find(".mh-datagrid-row-image");
-        rowData.attr("ng-if","column.type == 'image'")
-        rowData.attr("ng-src", "{{ row[column.modelKey] }}")
-
-        var selectAll = elem.find(".mh-datagrid-col-select-all");
-        selectAll.attr("ng-if","config.enableRowSelect !== false");
-
-        var selectRow = elem.find(".mh-datagrid-col-select");
-        selectRow.attr("ng-if","config.enableRowSelect !== false");
-
-        var selectAllCheckbox = elem.find(".mh-datagrid-select-all-checkbox");
-        selectAllCheckbox.attr("ng-model","asd");
-        selectAllCheckbox.attr("ng-change","controller.selectAll()");
-
-        var selectCheckbox = elem.find(".mh-datagrid-select-checkbox");
-        
-        return {
-            post : function(scope, el, attrs, ctrl, transclude)
-            {
-                transclude(function(elem)
-                {
-                    var elem = angular.copy(elem);
-                    var row = elem.find(".mh-datagrid-row");
-                    row.attr("ng-repeat", "row in config.collection");
-
-                    var col = elem.find(".mh-datagrid-col");
-                    col.attr("ng-repeat", "column in config.columns");
-
-                    var columnName = elem.find(".mh-datagrid-col-name");
-                    columnName.html("{{column.name}}");
-
-                    var rowData = elem.find(".mh-datagrid-row-text");
-                    rowData.attr("ng-if","column.type != 'image'")
-                    rowData.html("{{controller.selectAllModel}}");
-
-                    var rowData = elem.find(".mh-datagrid-row-image");
-                    rowData.attr("ng-if","column.type == 'image'")
-                    rowData.attr("ng-src", "{{ row[column.modelKey] }}")
-
-                    var selectAll = elem.find(".mh-datagrid-col-select-all");
-                    selectAll.attr("ng-if","config.enableRowSelect !== false");
-
-                    var selectRow = elem.find(".mh-datagrid-col-select");
-                    selectRow.attr("ng-if","config.enableRowSelect !== false");
-
-                    var selectAllCheckbox = elem.find(".mh-datagrid-select-all-checkbox");
-                    selectAllCheckbox.attr("ng-model","asd");
-                    selectAllCheckbox.attr("ng-change","controller.selectAll()");
-
-                    var selectCheckbox = elem.find(".mh-datagrid-select-checkbox");
-                    $compile(elem)(scope);
-                    el.html(elem);
-                }); 
-            }
-        }
-
-    },
-    controller: 'MHDatagridCtrl',
-    controllerAs : 'controller'
-  };
-});
-
-angular.module('mahou').directive('mhDatagridTemplate', function ( $compile ) {
-  return {
-    restrict: 'E',
-    transclude : true,
-    link : function(scope, el, attrs, ctrl, transclude)
-    {
-        console.log(scope);
         transclude(function(elem)
         {
             var elem = angular.copy(elem);
@@ -121,16 +39,8 @@ angular.module('mahou').directive('mhDatagridTemplate', function ( $compile ) {
             $compile(elem)(scope);
             el.html(elem);
         }); 
-    }
-  };
-});
-
-angular.module('mahou').directive('mhTemplateUrl', function ( $compile ) {
-  return {
-    restrict: 'A',
-    templateUrl : function(el, attrs)
-    {
-        return attrs.mhTemplateUrl;
-    }
+    },
+    controller: 'MHDatagridCtrl',
+    controllerAs : 'controller'
   };
 });
